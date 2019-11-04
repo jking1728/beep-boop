13e51d6a78a4a0e21bd786586d637ed26e9810f2
Jordan King <supapeachx@gmail.com>
2019-11-03 22:23:47 -0800

    updated logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 var numberset = [];
-for (var index = 1; index <= inputnumber; index += 1) {
-    numberset.push(index);
-};
 
 
 $(document).ready(function() {
     $("form#input").submit(function(event) {
         event.preventDefault();
-        var inputnumber = parseInt($("input#usernumber").val());
+        var inputnumber = $("input#usernumber").val();
         $("ul#output").empty("");
-        
+        for (var index = 1; index <= inputnumber; index += 1) {
+            numberset.push((index).toString());
+        };
         
 
-        for (var i = 0; i <= numberset.length -1; index += 1) {
-            if (numberset[i].includes('3') = true) {
+        for (var i = 0; i <= numberset.length -1; i += 1) {
+            if (numberset[i].includes('3') === true) {
                 $("ul#output").append("<li>" + "I'm sorry, Dave. I'm afraid I can't do that." + "</li>")
-            } else if (numberset[i].includes('2') = true) {
+            } else if (numberset[i].includes('2') === true) {
                 $("ul#output").append("<li>" + "Boop!" + "</li>")
-            } else if (numberset[i].includes('1') = true) {
+            } else if (numberset[i].includes('1') === true) {
                 $("ul#output").append("<li>" + "Beep!" + "</li>")
             } else {
                 $("ul#output").append("<li>" + numberset[i] + "</li>")
