c6aab0bfcb563cd4cf4df3adc3f1782e463bbe92
Jordan King <supapeachx@gmail.com>
2019-11-13 00:16:02 -0800

    updated logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var beep = " Beep! "
-var boop = " Boop! "
-var sorry = " I'm sorry, Dave. I'm afraid I can't do that. "
+var beep = "Beep!"
+var boop = "Boop!"
+var sorry = "I'm sorry, Dave. I'm afraid I can't do that."
 
 var numberset = [];
 
