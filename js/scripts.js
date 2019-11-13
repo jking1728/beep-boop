17991ea66648f6e50e2356d439a8b1c3c4b849cf
Jordan King <supapeachx@gmail.com>
2019-11-13 00:14:23 -0800

    updated logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,40 @@
+var beep = " Beep! "
+var boop = " Boop! "
+var sorry = " I'm sorry, Dave. I'm afraid I can't do that. "
+
 var numberset = [];
+
+function numberSpread(number) {
+  for (i=1; i <= number; i++) {
+    numberset.push(i.toString());
+  }
+}
+
+function transformArray() {
+    results = numberset.map(function(number) {
+      if (number.includes('3')){
+        return(sorry)
+      } else if (number.includes('2')) {
+        return (boop)
+      } else if (number.includes('1')) {
+        return (beep)
+      } else {
+        return (number)
+      }
+    });
+  }
 
 
 $(document).ready(function() {
     $("form#input").submit(function(event) {
         event.preventDefault();
-        var inputnumber = $("input#usernumber").val();
-        $("ul#output").empty("");
-        for (var index = 1; index <= inputnumber; index += 1) {
-            numberset.push((index).toString());
-        };
-        
-
-        for (var i = 0; i <= numberset.length -1; i += 1) {
-            if (numberset[i].includes('3') === true) {
-                $("ul#output").append("<li>" + "I'm sorry, Dave. I'm afraid I can't do that." + "</li>")
-            } else if (numberset[i].includes('2') === true) {
-                $("ul#output").append("<li>" + "Boop!" + "</li>")
-            } else if (numberset[i].includes('1') === true) {
-                $("ul#output").append("<li>" + "Beep!" + "</li>")
-            } else {
-                $("ul#output").append("<li>" + numberset[i] + "</li>")
-            }
-        };
+        var inputNumber = parseInt($("input#usernumber").val());
+        var userNumbers = numberSpread(inputNumber); 
+        transformArray();
+        results.forEach(function(result) {
+            $("#output").append("<li>" + result + "</li>");
+        });
+        numberset = []; 
         
     });
 });
